361ef653089cbdce2456716bad65dc6efcd918d3
Egor Nazarov <nazarovegorka@yandex.ru>
2024-12-12 17:31:17 +0300

    Sprint #21 - add shopping store

---
+++ commerce/shopping-store/src/main/java/ru/practicum/yandex/mapper/ProductMapper.java
@@ -0,0 +1,18 @@
+package ru.practicum.yandex.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
+import ru.practicum.yandex.model.Product;
+import ru.yandex.practicum.dto.ProductDto;
+
+import java.util.List;
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+public interface ProductMapper {
+
+	Product productDtoToProduc(ProductDto productDto);
+
+	ProductDto productToProductDto(Product product);
+
+	List<ProductDto> mapListProducts(List<Product> products);
+}
